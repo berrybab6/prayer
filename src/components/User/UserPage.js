c808ad79bf0b44ad34d0072bb953c49aa6d48084
berrybab6 <bedatuassefa@gmail.com>
2021-05-25 21:25:17 +0300

    display User Picture

--- src/components/User/UserPage.js
+++ src/components/User/UserPage.js
@@ -2,18 +2,22 @@ import React, { useState} from 'react';
 
 import{ Image } from 'react-bootstrap';
 
+import { useHistory } from 'react-router';
+
 import './users.css';
 function UserPage({users}) {
     const [name, setName] = useState(null);    
+    const history = useHistory();
 
     function changeName(){
         setName("Mish");
+        history.push('/home');
     }    
     return users.map((user, index)=>(
          <div className="user" key={index} onClick={changeName}>
-           <Image src='/images/sea-1337565__340.webp' rounded className="userPic" key={user.id}/>
+           <Image src={'http://127.0.0.1:8000/users/images/'+user.email+'/'} rounded className="userPic" key={user.id}/>
             <div className="userInfo" >
-            <div className="userInfo fullName">{name===null?user.full_name:name}</div>
+            <div className="userInfo fullName">{user.full_name}</div>
             <div className="userInfo email">
                 {user.email}
             </div>
